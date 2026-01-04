e2905375306a03aa5570177d0e8eeae4df3b274a
sami <samiyazuu@gmail.com>
2026-01-04 23:43:52 +0100

    firebase addition

---
+++ config.js
@@ -0,0 +1,9 @@
+// Firebase config provided by user. Consider NOT committing this file to a public repo.
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBsm0DDqq6nhmpQbqKy9-Twfg7DiAwomV8",
+  authDomain: "rybnik-leaderboard.firebaseapp.com",
+  projectId: "rybnik-leaderboard",
+  storageBucket: "rybnik-leaderboard.firebasestorage.app",
+  messagingSenderId: "842301438914",
+  appId: "1:842301438914:web:3b804b7ff0bf58b1b2745b"
+};
